5a6cc5811468e006c159df7678c99ef901191984
hi-it-isDebayan <debayandasxb@gmail.com>
2026-05-18 08:37:15 +0000

    Fix Python 3.10: fallback for tomllib not found

--- src/version_controller/_config.py
+++ src/version_controller/_config.py
@@ -1,12 +1,19 @@
 import os
-import tomllib
+
+try:
+    import tomllib
+except ModuleNotFoundError:
+    try:
+        import tomli as tomllib
+    except ModuleNotFoundError:
+        tomllib = None
 
 _MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
 _CONFIG_PATH = os.path.join(_MODULE_DIR, "config", "controller_config.toml")
 
 
 def load_config() -> dict:
-    if not os.path.isfile(_CONFIG_PATH):
+    if tomllib is None or not os.path.isfile(_CONFIG_PATH):
         return {}
     with open(_CONFIG_PATH, "rb") as f:
         return tomllib.load(f)
